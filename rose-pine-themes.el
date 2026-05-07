064b9f3a6fa5bd71881df9a74cd654a864c872af
lean <90573264+unixside@users.noreply.github.com>
2026-05-07 08:36:50 -0300

    Update rose-pine-themes.el
    Added dependencies.

--- rose-pine-themes.el
+++ rose-pine-themes.el
@@ -1,5 +1,5 @@
 ;;; rose-pine-themes.el --- -*- lexical-binding: t; -*-
-
+;; Package-Requires: ((modus-themes "5.2.0") (emacs "27.1"))
 ;;;###autoload
 (when load-file-name
   (let ((dir (file-name-directory load-file-name)))
